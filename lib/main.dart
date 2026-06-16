e85c561553fa008ca278fd44daea2341d4970b43
Ahmed Khodary <ahmedmohamed2019.com@gmail.com>
2026-06-17 01:06:25 +0300

    Add Windows runner support with DPI awareness and console output
    - Created a manifest file for the Windows runner to enable PerMonitorV2 DPI awareness and support for Windows 10 and 11.
    - Implemented utility functions in utils.cpp and utils.h for console creation and command line argument handling.
    - Developed the Win32Window class in win32_window.cpp and win32_window.h to manage high DPI-aware windows, including theme updates based on system preferences.

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FloweryRiderApp());
+}
+
+class FloweryRiderApp extends StatelessWidget {
+  const FloweryRiderApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Flutter Demo'));
+  }
+}
