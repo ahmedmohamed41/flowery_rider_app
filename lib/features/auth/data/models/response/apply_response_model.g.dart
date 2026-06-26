a45057578184b6961fbee7dc763ad6ac7879b52f
Ahmed Khodary <ahmedmohamed2019.com@gmail.com>
2026-06-27 00:40:03 +0300

    feat: Add AuthApiClient and ApplyResponseModel for driver application

---
+++ lib/features/auth/data/models/response/apply_response_model.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'apply_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ApplyResponseModel _$ApplyResponseModelFromJson(Map<String, dynamic> json) =>
+    ApplyResponseModel(message: json['message'] as String?);
+
+Map<String, dynamic> _$ApplyResponseModelToJson(ApplyResponseModel instance) =>
+    <String, dynamic>{'message': instance.message};
